ab1f863f2c2c3dc7e92bb1d8d6a181a2e0c0740c
codertoday <pranati.jayanty@vit.edu.in>
2023-05-09 09:39:03 +0530

    booktable

--- amplify/backend/function/ordercreator/src/index.js
+++ amplify/backend/function/ordercreator/src/index.js
@@ -2,9 +2,9 @@ const { v4: uuidv4 } = require("uuid");
 const AWS = require("aws-sdk");
 const documentClient = new AWS.DynamoDB.DocumentClient();
 
-const ORDER_TABLE = "<order_table_name>";
+const ORDER_TABLE = "Order-mdibieuhkngn3nhddzbqogeukq-prod";
 const ORDER_TYPE = "Order";
-const BOOK_ORDER_TABLE = "<book_order_table name>";
+const BOOK_ORDER_TABLE = "BookOrder-mdibieuhkngn3nhddzbqogeukq-prod";
 const BOOK_ORDER_TYPE = "BookOrder";
 
 const createOrder = async (payload) => {
